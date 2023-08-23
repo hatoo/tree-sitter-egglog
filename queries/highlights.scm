bc4b0f47d5a4a3908fd91ad03e0b4ec80499099c
hatoo <hato2000@gmail.com>
2023-08-24 07:11:37 +0900

    highlight

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,1 +1,11 @@
-"relation" @keyword
+[(lparen) (rparen)] @punctuation.bracket
+["set-option" "datatype" "sort" "function" "declare" "relation" "ruleset" "rule" "rewrite" "birewrite" "let" "run" "simplify" "add-ruleset" "calc" "query-extract" "check" "check-proof" "run-schedule" "push" "pop" "print-table" "print-size" "input" "output" "fail" "include"] @keyword
+(comment) @comment
+(callexpr (lparen) (ident) @function (expr) @variable.parameter) 
+
+; TODO
+; (command (lparen) "relation" (ident) @function)
+
+[(num) (unum)] @number
+(string) @string
+(type) @type
