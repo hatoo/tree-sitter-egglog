895ce7e5306b4626c95d9d8b0f9400fdab5743e6
hatoo <hato2000@gmail.com>
2023-08-23 13:14:57 +0900

    prototype grammar

--- grammar.js
+++ grammar.js
@@ -7,7 +7,7 @@ module.exports = grammar({
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: ($) => $.expr,
+    source_file: ($) => repeat($.command),
 
     lparen: ($) => choice("(", "["),
     rparen: ($) => choice(")", "]"),
@@ -37,10 +37,101 @@ module.exports = grammar({
           optional($.cost),
           optional(":unextractable"),
           optional(seq(":on_merge", list($, $.action))),
-          optional(":merge", $.expr),
+          optional(seq(":merge", $.expr)),
           $.rparen
         ),
-        seq($.lparen, "declare", $.ident, $.ident, $.rparen)
+        seq($.lparen, "declare", $.ident, $.ident, $.rparen),
+        seq($.lparen, "relation", $.ident, list($, $.type), $.rparen),
+        seq($.lparen, "ruleset", $.ident, $.rparen),
+        seq(
+          $.lparen,
+          "rule",
+          list($, $.fact),
+          list($, $.action),
+          optional(seq(":ruleset", $.ident)),
+          optional(seq(":name", $.string)),
+          $.rparen
+        ),
+        seq(
+          $.lparen,
+          "rewrite",
+          $.expr,
+          $.expr,
+          optional(seq(":when", list($, $.fact))),
+          optional(seq(":ruleset", $.ident)),
+          $.rparen
+        ),
+        seq(
+          $.lparen,
+          "birewrite",
+          $.expr,
+          $.expr,
+          optional(seq(":when", list($, $.fact))),
+          optional(seq(":ruleset", $.ident)),
+          $.rparen
+        ),
+        seq($.lparen, "let", $.ident, $.expr, $.rparen),
+        $.nonletaction,
+        seq(
+          $.lparen,
+          "run",
+          $.unum,
+          optional(seq(":until", repeat($.fact))),
+          $.rparen
+        ),
+        seq(
+          $.lparen,
+          "run",
+          $.ident,
+          $.unum,
+          optional(seq(":until", repeat($.fact))),
+          $.rparen
+        ),
+        seq($.lparen, "simplify", $.schedule, $.expr, $.rparen),
+        seq($.lparen, "add-ruleset", $.ident, $.rparen),
+        seq(
+          $.lparen,
+          "calc",
+          $.lparen,
+          repeat($.identsort),
+          $.rparen,
+          repeat1($.expr),
+          $.rparen
+        ),
+        seq(
+          $.lparen,
+          "query-extract",
+          optional(seq(":variants", $.unum)),
+          $.fact,
+          $.rparen
+        ),
+        seq($.lparen, "check", repeat($.fact), $.rparen),
+        seq($.lparen, "check-proof", $.rparen),
+        seq($.lparen, "run-schedule", repeat($.schedule), $.rparen),
+        seq($.lparen, "push", optional($.unum), $.rparen),
+        seq($.lparen, "pop", optional($.unum), $.rparen),
+        seq($.lparen, "print-table", $.ident, optional($.unum), $.rparen),
+        seq($.lparen, "print-size", $.ident, $.rparen),
+        seq($.lparen, "input", $.ident, $.string, $.rparen),
+        seq($.lparen, "output", $.string, repeat1($.expr), $.rparen),
+        seq($.lparen, "fail", $.command, $.rparen),
+        seq($.lparen, "include", $.string, $.rparen)
+      ),
+
+    schedule: ($) =>
+      choice(
+        seq($.lparen, "saturate", repeat($.schedule), $.rparen),
+        seq($.lparen, "seq", repeat($.schedule), $.rparen),
+        seq($.lparen, "repeat", $.unum, repeat($.schedule), $.rparen),
+        seq($.lparen, "run", optional(seq(":until", repeat($.fact))), $.rparen),
+        seq(
+          $.lparen,
+          "run",
+          $.ident,
+          optional(seq(":until", repeat($.fact))),
+          $.rparen
+        ),
+        seq($.ident)
       ),
 
     cost: ($) => seq(":cost", $.unum),
@@ -76,6 +167,11 @@ module.exports = grammar({
     action: ($) =>
       choice(seq($.lparen, "let", $.ident, $.expr, $.rparen), $.nonletaction),
 
+    name: ($) => seq("[", $.ident, "]"),
+
+    fact: ($) =>
+      choice(seq($.lparen, "=", repeat1($.expr), $.expr, $.rparen), $.expr),
+
     schema: ($) => seq(list($, $.type), $.type),
 
     expr: ($) => choice($.literal, $.ident, $.callexpr),
@@ -90,6 +186,7 @@ module.exports = grammar({
 
     type: ($) => choice($.ident),
 
+    identsort: ($) => seq($.lparen, $.ident, $.type, $.rparen),
     num: ($) => /(-)?[0-9]+/,
     unum: ($) => /[0-9]+/,
     f64: ($) =>
