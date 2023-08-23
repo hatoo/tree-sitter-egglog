36bac34da4ec9d08fe1dd969b9a8905c37753583
hatoo <hato2000@gmail.com>
2023-08-24 07:57:49 +0900

    Update highlight

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,10 +1,14 @@
 [(lparen) (rparen)] @punctuation.bracket
 ["set-option" "datatype" "sort" "function" "declare" "relation" "ruleset" "rule" "rewrite" "birewrite" "let" "run" "simplify" "add-ruleset" "calc" "query-extract" "check" "check-proof" "run-schedule" "push" "pop" "print-table" "print-size" "input" "output" "fail" "include"] @keyword
-(comment) @comment
-(callexpr (lparen) (ident) @function (expr) @variable.parameter) 
+["set" "delete" "union" "panic" "extract" "let"] @keyword
+[":unextractable" ":on_merge" ":merge" ":default" ":ruleset" ":name" ":when" ":until" ":variants" ":cost"] @attribute
 
-; TODO
-; (command (lparen) "relation" (ident) @function)
+(comment) @comment
+(expr (ident) @variable.parameter)
+(callexpr (lparen) (ident) @function)
+
+(command (ident) @function)
+(variant (ident) @function)
 
 [(num) (unum)] @number
 (string) @string
