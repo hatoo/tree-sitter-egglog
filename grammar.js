e4c617fa3e16df63877bc17b49e0a68693e41dc8
hatoo <hato2000@gmail.com>
2023-08-23 11:27:33 +0900

    format

--- grammar.js
+++ grammar.js
@@ -1,41 +1,33 @@
 module.exports = grammar({
-  name: 'egglog',
+  name: "egglog",
 
-  extras: $ => [
-    /\s/,
-    /;[^\n\r]*[\n\r]*/
-  ],
+  extras: ($) => [/\s/, /;[^\n\r]*[\n\r]*/],
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => $.expr,
+    source_file: ($) => $.expr,
 
-    lparen: $ => choice("(", "["),
-    rparen: $ => choice(")", "]"),
+    lparen: ($) => choice("(", "["),
+    rparen: ($) => choice(")", "]"),
 
-    list: $ => seq($.lparen, repeat($.expr), $.rparen),
-    comma: $ => repeat1(seq($.expr, ",")),
+    list: ($) => seq($.lparen, repeat($.expr), $.rparen),
+    comma: ($) => repeat1(seq($.expr, ",")),
 
-    command: $ => choice(
-      seq($.lparen, "set-option", $.ident, $.expr, $.rparen),
-    ),
+    command: ($) =>
+      choice(seq($.lparen, "set-option", $.ident, $.expr, $.rparen)),
 
-    expr: $ => choice($.ident, $.literal, $.callexpr),
+    expr: ($) => choice($.ident, $.literal, $.callexpr),
 
-    literal : $ => choice($.num, $.f64, $.symstring),
+    literal: ($) => choice($.num, $.f64, $.symstring),
 
-    callexpr: $ => seq($.lparen, $.ident, repeat($.expr), $.rparen),
+    callexpr: ($) => seq($.lparen, $.ident, repeat($.expr), $.rparen),
 
-    num: $ => /(-)?[0-9]+/,
-    unum: $ => /[0-9]+/,
-    f64: $ => choice(
-      "NaN",
-      /(-)?[0-9]+\.[0-9]+(e(\+)?(-)?[0-9]+)?/,
-      "inf",
-      "-inf"
-    ),
-    ident: $ => /(([[a-zA-z_]][\w-]*)|([-+*/?!=<>&|^/%_]))+/,
-    symstring: $ => $.string,
-    string: $ => /"("[^"]*")+"/
-  }
+    num: ($) => /(-)?[0-9]+/,
+    unum: ($) => /[0-9]+/,
+    f64: ($) =>
+      choice("NaN", /(-)?[0-9]+\.[0-9]+(e(\+)?(-)?[0-9]+)?/, "inf", "-inf"),
+    ident: ($) => /(([[a-zA-z_]][\w-]*)|([-+*/?!=<>&|^/%_]))+/,
+    symstring: ($) => $.string,
+    string: ($) => /"("[^"]*")+"/,
+  },
 });
