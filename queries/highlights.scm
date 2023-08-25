8838d580b8d69310b0b8419d421c8b94e2af8bb5
hatoo <hato2000@gmail.com>
2023-08-25 21:42:51 +0900

    Update highlights
    - Stil remains unknown words to me

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,15 +1,19 @@
 [(lparen) (rparen)] @punctuation.bracket
 ["set-option" "datatype" "sort" "function" "declare" "relation" "ruleset" "rule" "rewrite" "birewrite" "let" "run" "simplify" "add-ruleset" "calc" "query-extract" "check" "check-proof" "run-schedule" "push" "pop" "print-table" "print-size" "input" "output" "fail" "include"] @keyword
 ["set" "delete" "union" "panic" "extract" "let"] @keyword
+(fact ("=" @keyword))
 [":unextractable" ":on_merge" ":merge" ":default" ":ruleset" ":name" ":when" ":until" ":variants" ":cost"] @attribute
 
 (comment) @comment
-(expr (ident) @variable.parameter)
-(callexpr (ident) @function)
+(callexpr ((ident) @function))
 
-(command (ident) @function)
-(variant (ident) @function)
+(command ("datatype" (ident) @type))
+(command ("relation" (ident) @function))
+(command ("function" (ident) @function))
+(command ((lparen) "declare" (ident) @variable (ident) @type (rparen)))
+(variant ((ident) @function))
 
+((ident) @variable)
 [(num) (unum)] @number
 (string) @string
 (type) @type
