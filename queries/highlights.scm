7c87d69814f0d4a9f1e0ad24a9ac7dd6f8cf5c5d
hatoo <hato2000@gmail.com>
2023-08-29 16:49:17 +0900

    add keyword

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,6 @@
 [(lparen) (rparen)] @punctuation.bracket
 ["set-option" "datatype" "sort" "function" "declare" "relation" "ruleset" "rule" "rewrite" "birewrite" "let" "run" "simplify" "add-ruleset" "calc" "query-extract" "check" "check-proof" "run-schedule" "push" "pop" "print-table" "print-size" "input" "output" "fail" "include"] @keyword
-["set" "delete" "union" "panic" "extract" "let"] @keyword
+["saturate" "set" "repeat" "delete" "union" "panic" "extract" "let"] @keyword
 (fact ("=" @keyword))
 [":unextractable" ":on_merge" ":merge" ":default" ":ruleset" ":name" ":when" ":until" ":variants" ":cost"] @attribute
 
